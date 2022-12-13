8f49d57f9a74b3b331d16d173b695df5ecf5bc6d
Eugene <rjsdj56@gmail.com>
2022-12-13 12:34:25 +0900

    1213 second

--- src/com/spring/ex02/LoginController.java
+++ src/com/spring/ex02/LoginController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -91,10 +92,11 @@ public class LoginController {
 
 		return mav;
 	}
-	@RequestMapping(value = "/test/login4.do", method = {RequestMethod.GET, RequestMethod.POST})
+
+	@RequestMapping(value = "/test/login4.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView login4(@ModelAttribute("info") LoginVO loginVO, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		request.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8"); 
 		ModelAndView mav = new ModelAndView();
 
 		System.out.println("userID : " + loginVO.getUserID());
@@ -105,5 +107,14 @@ public class LoginController {
 		return mav;
 
 	}
-
+	@RequestMapping(value="/test/login5.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public String login5(Model model,
+					     HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("UTF-8"); 
+		
+		model.addAttribute("userID", "hong");
+		model.addAttribute("userName", "홍길동");
+		
+		return "result";
+	}
 }
